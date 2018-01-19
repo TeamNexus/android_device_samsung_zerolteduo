648eda6ed49843e55445490df7a555c218f4b562
Lukas0610 <mail@lukasberger.at>
2018-01-19 11:37:07 +0100

    zerolteduo: correct OTA-assert

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -24,7 +24,7 @@
 include device/samsung/zero-common/BoardConfigCommon.mk
 
 # Assert
-TARGET_OTA_ASSERT_DEVICE := zerolteduo
+TARGET_OTA_ASSERT_DEVICE := zerolteduo,zeroltexx,zerolte
 
 # Include path
 TARGET_SPECIFIC_HEADER_PATH += device/samsung/zerolteduo/include
