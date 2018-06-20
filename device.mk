f4960a1f8db2874b14f7ce6d4c768cc478305ffd
Lukas0610 <mail@lukasberger.at>
2018-06-20 08:34:39 +0200

    zerolteduo: remove reference to deprecated zero-non-spr vendor repo

--- device.mk
+++ device.mk
@@ -25,8 +25,5 @@ BOARD_HAS_DUAL_SIM := true
 # Inherit model-specific blobs
 $(call inherit-product, vendor/samsung/zero-duo/zero-duo-vendor.mk)
 
-# Inherit semi-common blobs
-$(call inherit-product, vendor/samsung/zero-non-spr/zero-non-spr-vendor.mk)
-
 # Inherit from zero-common
 $(call inherit-product, device/samsung/zero-common/zero-common.mk)
