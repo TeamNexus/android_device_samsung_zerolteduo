4f9a1871cbd2818843652e8ba9b816cc67f02876
Lukas0610 <mail@lukasberger.at>
2018-09-07 21:33:39 +0200

    zerolteduo: rename lineage product makefile to support Pie

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -3,6 +3,6 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/aokp.mk \
     $(LOCAL_DIR)/aosp_zerolteduo.mk \
     $(LOCAL_DIR)/gzosp.mk \
-    $(LOCAL_DIR)/lineage.mk \
+    $(LOCAL_DIR)/lineage_zerolteduo.mk \
     $(LOCAL_DIR)/nexus_zerolteduo.mk \
     $(LOCAL_DIR)/omni_zerolteduo.mk
